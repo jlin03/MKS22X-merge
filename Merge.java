ac422f2db29652dfc4867545366951ee02a10151
jlin03 <jlin03@stuy.edu>
2019-03-15 14:36:57 -0400

    clone written, mergesort main written

--- Merge.java
+++ Merge.java
@@ -1,16 +1,27 @@
+import java.util.*;
 public class Merge {
 
   public static void mergesort(int[] data) {
-    return;
+    int[] temp = clone(data);
+    mSH(data,temp,0,data.length-1);
   }
 
-  public static void mSH(int[] data, int l, int h) {
+  private static void mSH(int[] data, int[] temp, int l, int h) {
+
+
   }
 
   private static void merge(int[] data, int[] left, int[] right) {
     return;
   }
 
+  private int[] clone(int[] data) {
+    int[] out = new int[data.length];
+    for(int i = 0; i < out.length; i++) {
+      out[i] = data[i];
+    }
+    return out;
+  }
 
 
 
