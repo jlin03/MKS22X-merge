f1b13e27d3c13718152c111e76c4ced752aaa95e
jlin03 <jlin03@stuy.edu>
2019-03-15 14:32:04 -0400

    work started for merge

---
+++ Merge.java
@@ -0,0 +1,20 @@
+public class Merge {
+
+  public static void mergesort(int[] data) {
+    return;
+  }
+
+  public static void mSH(int[] data, int l, int h) {
+  }
+
+  private static void merge(int[] data, int[] left, int[] right) {
+    return;
+  }
+
+
+
+
+
+
+
+}
